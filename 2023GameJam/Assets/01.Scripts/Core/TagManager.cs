6104d5e312902378297d05212cf6d4ca8e83afa6
Gusdnd01 <cestop42@naver.com>
2023-01-09 00:24:32 +0900

    Main

---
+++ 2023GameJam/Assets/01.Scripts/Core/TagManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TagManager
+{
+    public static string TAG_PLAYER = "Player";
+    public static string TAG_ENEMY = "Enemy";
+    public static string TAG_GROUND = "Ground";
+}
