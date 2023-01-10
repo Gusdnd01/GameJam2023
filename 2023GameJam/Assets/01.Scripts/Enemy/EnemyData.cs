be0d202d781517250c0d003bd287dd2b11db635d
Gusdnd01 <cestop42@naver.com>
2023-01-10 22:47:32 +0900

    파티클 추가함

--- 2023GameJam/Assets/01.Scripts/Enemy/EnemyData.cs
+++ 2023GameJam/Assets/01.Scripts/Enemy/EnemyData.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Enemy/EnemyData")]
 public class EnemyData : ScriptableObject
 {
-   public float hp = 10f;
    public float speed = 5f;
 
    public float attackDistance = 2f;
+   public float checkDistance = 2f;
    public float cycleDelay = 1f;
 }
